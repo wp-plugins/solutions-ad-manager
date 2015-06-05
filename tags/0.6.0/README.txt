99b7d15e888fd809ed4ec3e38b02df3fe6031208
solutionsbysteve <solutionsbysteve@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-05 01:24:33 +0000

    First release on Wordpress Repo
    git-svn-id: https://plugins.svn.wordpress.org/solutions-ad-manager@1174910 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/0.6.0/README.txt
@@ -0,0 +1,117 @@
+=== Plugin Name ===
+Contributors: solutionsbysteve 
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=CZEPP9YUEPMW2&lc=US&item_name=Solutions%20By%20Steve&item_number=SoultionsAdManager&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted
+Tags: ads, widgets, shortcodes
+Requires at least: 3.0.1
+Tested up to: 4.2.2
+Stable tag: 4.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A simple ad manager that allows you to display pictures and YouTube videos in to a widget.
+
+== Description ==
+
+Solutions Ad Manager is a simple ad manager that allows you to display advertisements in the form of pictures, YouTube videos, and many more embeddable elements in to a widget. This plugin comes with builtin analytics to track how many clicks you get for each ad.
+
+This plugin is free to use and comes with the GPLv2 or later license. With that said this plugin lives solely on donations. If there is no monetary value in coding this plugin than development will cease to exist. If you would like you can donate here or use the button below.
+
+If you would like to make requests for specific features please feel free to do so at http://solutionsbysteve.com/contact/ Remember, these are simply requests and no guarantee that it will fit in to the scope of the plugin but will absolutely be taken into consideration.
+
+Chances are you are selling these ad spaces to people on your website. If you are making money of off my hard work and dedication then maybe you would like to share a little of the appreciation with the developer.
+
+**Donations**
+Please feel free to make feature requests with your donations. DO NOTE: these are simply requests and no guarantee that it will fit in to the scope of the plugin but will absolutely be taken into consideration.
+
+https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=CZEPP9YUEPMW2&lc=US&item_name=Solutions%20By%20Steve&item_number=SoultionsAdManager&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted
+
+
+**Expectations for Feature Releases**
+Shortcodes - WIP
+Better Analytics and Integration with Google Analytics
+Custom Styles
+
+
+== Installation ==
+
+1. Extract `solutions-ad-manager.zip` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Display your ads by editing your widgets
+
+== Frequently Asked Questions ==
+
+= How to use Solutions Ad Manager =
+Create an Ad
+Create a Group
+Add your ads to the Group
+Select Widgets under Appearance
+Choose the widget you would like to display
+How to show titles and controls on YouTube videos
+Select Options.
+Choose the options you would like under "YouTube Options".
+Press Save
+
+= How to show titles and controls on YouTube videos =
+Select Options.
+Choose the options you would like under "YouTube Options".
+Press Save
+
+= How to backup your ads =
+Ads can be backed up by using the Export feature that comes with WordPress. And if you need to restore them use the Import feature that comes with WordPress.
+
+= How to make feature requests =
+If you would like to make requests for specific features please feel free to do so at http://solutionsbysteve.com/contact/ Remember, these are simply requests and no guarantee that it will fit in to the scope of the plugin but will absolutely be taken into consideration.
+
+= How to Donate = 
+Donations are the foundation of this plugin, without them it will cease to exist. Thank you for your consideration and it is truly appreciated.
+https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=CZEPP9YUEPMW2&lc=US&item_name=Solutions%20By%20Steve&item_number=SoultionsAdManager&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted
+
+== Screenshots ==
+
+1. Admin page for creating an ad, you will notice it looks familiar because it uses WordPress's Custom Post Types
+
+== Changelog ==
+
+= 0.6 =
+* Prep for Release
+
+= 0.5 =
+* Remove thumbnails from admin column view
+* Fix Group Display on widgets
+
+= 0.4 =
+* Fixed Settings Name
+* Fixed Sort by Clicks
+* Made custom embed more OOP
+
+= 0.3 =
+* Fixed includes
+* Fixed featured image
+
+= 0.2 =
+* Support for YouTube videos
+* Add options page
+* Specific Ad Widget
+
+= 0.1 =
+* Beta Release.
+
+== Upgrade Notice ==
+
+= 0.6 =
+0.6 is the first public release and submission for wordpress repo, nothing fancy here but might as well update.
+
+= 0.5 =
+0.5 introduces Cosmetic Fixes, not required to update.
+
+= 0.4 =
+0.4 introduces Major Fixes, you should update.
+
+= 0.3 =
+0.3 introduces Major Fixes, you should update.
+
+= 0.2 =
+0.2 Release adds support for YouTube videos.
+
+= 0.1 =
+Beta Release.
---
+++ tags/0.6.0/includes/class-solutions-ad-manager-widget.php
@@ -0,0 +1,181 @@
+<?php 
+
+/**
+ * Adds Solutions_Ad_Manager_Random_From_Group_Widget widget.
+ */
+class Solutions_Ad_Manager_Random_From_Group_Widget extends WP_Widget {
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		$this->solutions_ad_manager = 'solutions-ad-manager';
+		$this->version = '0.6.0';
+		parent::__construct(
+			'sam_random_from_group_widget', // Base ID
+			__( 'Ad (random from group)', 'solutions-ad-manager' ), // Name
+			array( 'description' => __( 'Grab a random ad from selected group', 'solutions-ad-manager' ), ) // Args
+		);
+	}
+	
+	/**
+	 * The name of the plugin used to uniquely identify it within the context of
+	 * WordPress and to define internationalization functionality.
+	 *
+	 * @since     0.1.0
+	 * @return    string    The name of the plugin.
+	 */
+	public function get_solutions_ad_manager() {
+		return $this->solutions_ad_manager;
+	}
+
+	/**
+	 * Retrieve the version number of the plugin.
+	 *
+	 * @since     0.1.0
+	 * @return    string    The version number of the plugin.
+	 */
+	public function get_version() {
+		return $this->version;
+	}
+	
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		
+		global $sam_ad_array;
+		$term = get_term( $instance['groupTax'], 'solutions-ad-group' );
+		$group = $term->slug;
+		$query_args = array( 
+			'post_type' => array('solutions-ad-manager'),
+			$term->taxonomy => $group,
+			'orderby' => 'rand',
+		);
+		
+		if( empty($sam_ad_array[$group]) || !$sam_ad_array[$group]->have_posts() ){
+			// Create new query
+			$sam_ad_array[$group] = new WP_Query( $query_args );
+		}
+		
+		$options = get_option( 'solutions-ad-manager-options' );
+		if(!isset( $options['solutions-ad-manager-stretch-image'] )){ 
+			$stretchImage = false;
+		}else{
+			$stretchImage = $options['solutions-ad-manager-stretch-image'];
+		}
+	
+		
+		if ( $sam_ad_array[$group]->have_posts() ) {
+			$sam_ad_array[$group]->the_post();
+			
+			echo $args['before_widget'];
+			echo '<div class="solutions-ad-manager-widget" id="samid-'.$sam_ad_array[$group]->post->ID.'">';
+			
+			$siteURL = home_url();
+			$meta = array();
+			$meta['URL'] = get_post_meta( $sam_ad_array[$group]->post->ID, 'solutions_ad_url', true );
+			$meta['title'] = get_the_title();
+			$meta['media'] = esc_html(get_post_meta( $sam_ad_array[$group]->post->ID, 'solutions_ad_oembed', true ));
+			if ( $instance['showTitle'] == 'on' && !empty($meta['title']) ) {
+				echo $args['before_title'];
+				if(!empty($meta['URL'])){echo '<a href="' . $meta['URL'] . '">';}
+				echo $meta['title'];
+				if(!empty($meta['URL'])){echo '</a>';}
+				echo $args['after_title'];
+			}
+			if ( !empty($meta['media']) ){
+				echo '<div class="media">';
+				$plugin_public = new Solutions_Ad_Manager_Public( $this->get_solutions_ad_manager(), $this->get_version() );
+				echo $plugin_public->sam_oembed($meta['media']);
+				echo '</div>';
+			}else{
+				echo '<div class="image">';
+				if(!empty($meta['URL'])){echo '<a href="' . esc_url($siteURL) . '?sam-redirect-to=' . esc_url( $meta['URL'] ) . '&sam-post-id=' . $sam_ad_array[$group]->post->ID . '" rel="nofollow">';}
+				if($stretchImage){
+					echo get_the_post_thumbnail( $sam_ad_array[$group]->post->ID,  'full', array('class' => 'stretch') );
+				}else{
+					echo get_the_post_thumbnail( $sam_ad_array[$group]->post->ID,  'full' );
+				}
+				if(!empty($meta['URL'])){echo '</a>';}
+				echo '</div>';
+			}
+			
+			echo '</div>';
+			echo $args['after_widget'];
+			
+		} else {
+			echo $args['before_widget'];
+			echo '<div class="solutions-ad-manager-widget">';
+			echo __( 'No Ad Found', 'solutions-ad-manager' );
+			echo '</div>';
+			echo $args['after_widget'];
+			
+		}
+		
+		
+		
+		
+		
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		//Default Widget Settings
+		$defaults = array(
+			'showTitle' => 'on'
+		);
+		$instance = wp_parse_args( (array) $instance, $defaults ); 
+
+		//Display Form
+		echo '<p>';
+		echo '<select id="' . $this->get_field_id('groupTax') . '" name="' . $this->get_field_name('groupTax') . '" class="widefat" style="width:100%;">';
+		foreach(get_terms('solutions-ad-group','parent=0&hide_empty=0') as $term) {
+			echo '<option ' . selected( $instance['groupTax'], $term->term_id ) . ' value="' . $term->term_id . '">' . $term->name . '</option>';
+		}
+		echo '</select>';
+		echo '</p>';
+		
+		echo '<p>';
+		echo '<input class="checkbox" type="checkbox"' . checked($instance['showTitle'], 'on', false) . ' id="' . $this->get_field_id('showTitle') . '" name="' . $this->get_field_name('showTitle') . '" />';
+		echo '<label for="'.$this->get_field_id('showTitle').'">' . __( 'Show Title', 'solutions-ad-manager' ) .'</label>';
+		echo '</p>';
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		
+		$instance = $old_instance;
+		// Fields
+		$instance['groupTax'] = $new_instance['groupTax'];
+		$instance['showTitle'] = $new_instance['showTitle'];
+		
+		return $instance;
+	}
+
+
+} // class Solutions_Ad_Manager_Random_From_Group_Widget
+
+
+?>
